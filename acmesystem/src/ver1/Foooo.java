4cfe72e9d213f7970f0ab5b1a7758600e0b668f1
Adam Alkatiri <adamalkatiri77@gmail.com>
2026-06-21 21:28:07 -0400

    This better work becuase this is the foooo project

---
+++ acmesystem/src/ver1/Foooo.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package ver1;
+
+/**
+ * 
+ */
+public class Foooo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
