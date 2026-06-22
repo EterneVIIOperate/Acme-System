77858466758ea42d85fe228199d2689d4058f1ff
Adam Alkatiri <adamalkatiri77@gmail.com>
2026-06-21 22:13:17 -0400

    Hey NOW!

--- acmesystem/src/ver1/HelloWorld.java
+++ acmesystem/src/ver1/HelloWorld.java
@@ -5,6 +5,15 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello from Eclipse");
 		System.out.println("Dave is working locally on his branch");
+	
+		 heyNow();
+    }
+
+    public static void heyNow() { 
+        String Msg = "Hey Now!"; 
+        System.out.println(Msg); 
+
+
 	}
 
 }
